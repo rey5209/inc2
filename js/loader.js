cadd147c0204c600531e6dae9cd537de71ed7d1c
Rey Ramos Tuñacao <rey52090@gmail.com>
2025-05-17 12:21:33 +0800

    -add loader

---
+++ js/loader.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+  setTimeout(function() {
+    document.getElementById('loader-overlay').classList.add('hide');
+  }, 400); // Optional: delay for smoothness
+});
